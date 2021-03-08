897a74bce6f43cdd077505e01dea429c5b8f64a5
mohsenrs <mohsen8022@gmail.com>
2021-03-08 15:23:50 +0330

    gfdry

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+      console.log("task", id);
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at school",
+        day: "March 3rd at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 3rd at 11:00am",
+        reminder: false,
+      },
+    ];
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 100%;
+  max-width: 700px;
+  margin: auto;
+  padding: 10px 20px;
+  border: 1px solid rgb(35, 112, 48);
+  border-radius: 10px;
 }
 </style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -19,4 +19,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+button {
+  padding: 8px 10px;
+  background: rgb(36, 194, 194);
+  border: none;
+  border-radius: 15px;
+  font-size: 16px;
+  font-weight: 550;
+}
+</style>
